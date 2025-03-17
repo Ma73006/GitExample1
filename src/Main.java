e0ed7ea27c3d04c664f065bbb34881b8229dcb36
Mateo Gomez Castillo <mateogomez@uninorte.edu.co>
2025-03-17 08:34:06 -0500

    Feat: Added Person class

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
+
+import core.Person;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author Prestamos G
  */
 public class Main {
-   
+
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        Person person = new Person("mateo", 17, 2567);
+        System.out.println(person);
     }
-    
+
 }
---
+++ src/core/Person.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core;
+
+/**
+ *
+ * @author mateogomez
+ */
+public class Person {
+    private String name;
+    private int age;
+    private int id;
+
+    public Person(String name, int age, int id) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "name= " + name + ", age= " + age + ", id= " + id + '}';
+    }
+    
+}
